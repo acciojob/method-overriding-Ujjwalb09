fd6e96ca5e7b3d252b3ac567bf1d795fb17821a6
Ujjwalb09 <ujjwalbhatt09@gmail.com>
2023-04-15 18:19:37 +0530

    project completed

---
+++ src/main/java/com/driver/ClassA.java
@@ -0,0 +1,8 @@
+package com.driver;
+
+public class ClassA {
+
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
---
+++ src/main/java/com/driver/Main.java
@@ -0,0 +1,10 @@
+package com.driver;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassB cb = new ClassB();
+
+        String str = cb.meth(); //calling method of class A with class B object
+        System.out.println(str);
+    }
+}
